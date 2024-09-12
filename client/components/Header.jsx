25dfb034d61d5f559fde93c035746ac80674cf38
Stephenanokz <asedinho2@gmail.com>
2024-09-12 12:42:30 +0100

    first commit

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { useState } from "react";
-import NavLink from "./navLink";
+import NavLink from "./NavLink";
 import { motion } from "framer-motion";
 
 const links = [
